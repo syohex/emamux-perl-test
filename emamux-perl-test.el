2bd2eaefa791a842e858acb29aba1395f904b4f4
Syohei YOSHIDA <syohex@gmail.com>
2012-07-31 20:59:47 +0900

    Refactoring.
    - Remove global variable as possible
    - Remove cl function for warnings at byte-compile.

--- emamux-perl-test.el
+++ emamux-perl-test.el
@@ -32,20 +32,11 @@
 
 (require 'emamux)
 
-(defvar emamux-pt:build-file-list
-  '("Makefile.PL" "Build" ".proverc"))
-
-(defvar emamux-pt:build-type nil)
-(defvar emamux-pt:top-directory nil)
-
 (defun emamux-pt:top-directory-p (dir)
   (let ((default-directory dir))
-    (find-if (lambda (f)
-               (file-exists-p f)) emamux-pt:build-file-list)))
-
-(defun emamux-pt:init ()
-  (setq emamux-pt:top-directory nil)
-  (setq emamux-pt:build-type nil))
+    (loop for file in '("Makefile.PL" "Build.PL" ".proverc")
+          when (file-exists-p file)
+          return t)))
 
 (defun emamux-pt:parent-dir (dir)
   (file-name-directory (replace-regexp-in-string "/$" "" dir)))
@@ -59,43 +50,73 @@
         (error "Are you in Perl module directory ?"))
     (file-name-as-directory dir)))
 
-(defun emamux-pt:test-all-command ()
-  (case emamux-pt:build-type
+(defun emamux-pt:test-all-command (build-type topdir)
+  (case build-type
     (makefile "make test")
     (build    "./Build test")
-    (prove    (emamux-pt:test-all-prove))
+    (prove    (emamux-pt:test-all-prove topdir))
     (otherwise
      (error "Never reach here"))))
 
-(defun emamux-pt:test-all-prove ()
-  (format "prove -v %s -r t/" (emamux-pt:lib-option)))
+(defun emamux-pt:test-all-prove (topdir)
+  (let ((default-directory topdir))
+    (let ((t-dir-p  (file-directory-p "t/"))
+          (xt-dir-p (file-directory-p "xt/")))
+      (unless (or t-dir-p xt-dir-p)
+        (error "No tests in your project!!"))
+      (format "prove -v %s -r %s %s"
+             (emamux-pt:lib-option topdir)
+             (or (and t-dir-p "t/")  "")
+             (or (and xt-dir-p "xt/") "")))))
 
-(defun emamux-pt:test-file-command (file)
-  (format "prove -v %s %s" (emamux-pt:lib-option) file))
+(defun emamux-pt:test-file-command (file topdir)
+  (format "prove -v %s %s" (emamux-pt:lib-option topdir) file))
 
-(defun emamux-pt:lib-option ()
-  (let ((default-directory emamux-pt:top-directory))
+(defun emamux-pt:lib-option (topdir)
+  (let ((default-directory topdir))
     (cond ((file-directory-p "blib") "-b")
           ((file-directory-p "lib")  "-l")
           (t ""))))
 
-(defun emamux-pt:get-build-type ()
-  (let ((default-directory emamux-pt:top-directory))
+(defun emamux-pt:concat-command (build-cmd test-cmd)
+  (format " %s %s"
+          (or (and build-cmd (concat build-cmd " && ")) "")
+          test-cmd))
+
+(defun emamux-pt:pre-command (type topdir)
+  (let ((default-directory topdir))
+    (case type
+      (makefile  "perl Makefile.PL")
+      (build     "perl Build.PL")
+      (otherwise nil))))
+
+(defun emamux-pt:built-p (type topdir)
+  (let ((default-directory topdir))
+    (or (and current-prefix-arg t)
+        (case type
+          (makefile (not (file-exists-p "Makefile")))
+          (build    (not (file-exists-p "Build")))
+          (otherwise nil)))))
+
+(defun emamux-pt:test-command (test-type build-type file topdir)
+  (if (eq test-type 'all)
+      (emamux-pt:test-all-command build-type topdir)
+    (emamux-pt:test-file-command file topdir)))
+
+(defun emamux-pt:build-type (topdir)
+  (let ((default-directory topdir))
     (cond ((file-exists-p "Makefile.PL") 'makefile)
-          ((file-exists-p "Build.PL") 'build)
+          ((file-exists-p "Build.PL")    'build)
           (t 'prove))))
 
-(defun emamux-pt:wrap-command (cmd)
-  (format " (cd %s && %s)" emamux-pt:top-directory cmd))
-
-(defun emamux-pt:run-test-common (type)
-  (emamux-pt:init)
-  (let ((emamux-pt:top-directory (emamux-pt:find-top-dir)))
-    (setq emamux-pt:build-type (emamux-pt:get-build-type))
-    (let ((cmd (if (eq type 'all)
-                   (emamux-pt:test-all-command)
-                 (emamux-pt:test-file-command (buffer-file-name)))))
-      (emamux:run-command (emamux-pt:wrap-command cmd)))))
+(defun emamux-pt:run-test-common (test-type)
+  (let* ((topdir (emamux-pt:find-top-dir))
+         (build-type (emamux-pt:build-type topdir))
+         (built-p (emamux-pt:built-p build-type topdir))
+         (build-cmd (and built-p (emamux-pt:pre-command build-type topdir)))
+         (file (file-relative-name (buffer-file-name) topdir))
+         (cmd (emamux-pt:test-command test-type build-type file topdir)))
+    (emamux:run-command (emamux-pt:concat-command build-cmd cmd) topdir)))
 
 (defun emamux-perl-test:run-all ()
   (interactive)
